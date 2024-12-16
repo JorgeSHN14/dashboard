fab67d9b317f5432a93b52c5831377b191936f0b
JorgeHerrera <joheniet@espol.edu.ec>
2024-12-16 09:51:06 -0500

    Guia 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
+/*import { useState } from 'react'*/
+/*import reactLogo from './assets/react.svg'*/
+/*import viteLogo from '/vite.svg'*/
 import './App.css'
-import Grid from '@mui/material/Grid2' 
+
+//GUÍA 11
+import Grid from '@mui/material/Grid2'
+
+//GUÍA 12
+import IndicatorWeather from './components/IndicatorWeather';
+
+  //Grid Anidado
+  import TableWeather from './components/TableWeather';
+  import ControlWeather from './components/ControlWeather';
+
+  import LineChartWeather from './components/LineChartWeather';
 
 function App() {
+  /*const [count, setCount] = useState(0)*/
 
   return (
     <Grid container spacing={5}>
 
-       	  {/* Indicadores */}
-           <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-           <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-           <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-           <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
-		      
-           {/* Tabla */}
-           <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
-		      
-           {/* Gráfico */}
-           <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
-		  
-       </Grid>
+        {/* Indicadores */}
+        <Grid size={{ xs: 12, xl: 3 }}> 
+          <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+        </Grid>
+        <Grid size={{ xs: 12, xl: 3 }}> 
+        <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+        </Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} /> 
+        </Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+        </Grid>
+
+        {/* Tabla */}
+        <Grid size={{ xs: 12, xl: 8 }}>
+
+          {/* Grid Anidado */}
+          <Grid container spacing={2}>
+            <Grid size={{ xs: 12, xl: 3 }}>
+              <ControlWeather/>
+            </Grid>
+
+            <Grid size={{ xs: 12, xl: 9 }}>
+              <TableWeather/>
+            </Grid>
+          </Grid>
+
+        </Grid>
+
+        {/* Gráfico */}
+        <Grid size={{ xs: 12, xl: 4 }}>
+          <LineChartWeather/>
+        </Grid>
+
+    </Grid>
   )
 }
 
